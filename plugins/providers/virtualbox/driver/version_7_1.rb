5a808ecea69d8b50e0d2ab58ee10bdb148499e2d
Francesco Paolo Lovergine <frankie@debian.org>
2024-09-26 17:51:24 +0200

    Added support for provider VirtualBox 7.1

---
+++ plugins/providers/virtualbox/driver/version_7_1.rb
@@ -0,0 +1,28 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: BUSL-1.1
+
+require "rexml"
+require File.expand_path("../version_7_0", __FILE__)
+
+module VagrantPlugins
+  module ProviderVirtualBox
+    module Driver
+      # Driver for VirtualBox 7.0.x
+      class Version_7_1 < Version_7_0
+        def initialize(uuid)
+          super
+
+          @logger = Log4r::Logger.new("vagrant::provider::virtualbox_7_0")
+        end
+
+        # VirtualBox version in use
+        #
+        # @return [Gem::Version]
+        def get_version
+          return @version if @version
+          @version = Gem::Version.new(Meta.new.version)
+        end
+      end
+    end
+  end
+end
